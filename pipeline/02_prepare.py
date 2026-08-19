cc69f313cbde07cca3a32e27cdd401792f987e7e
BBongCoding <ihhan01@gmail.com>
2026-08-19 15:42:51 +0900

    진행 바 추가

--- pipeline/02_prepare.py
+++ pipeline/02_prepare.py
@@ -1,6 +1,7 @@
 import sqlite3
 import statistics
 import sys
+import tqdm
 from pathlib import Path
 
 sys.path.insert(0,str(Path(__file__).resolve().parent.parent))
@@ -62,7 +63,7 @@ if __name__ == "__main__":
     # 글에서 ## 제품소개, ## 주요성분 같은 2단계 제목을 발견할 때 마다 본문을 분리해서 저장할 빈 리스트 생성
     sections = []
 
-    for pid, pname, detail in details:
+    for pid, pname, detail in tqdm.tqdm(details, desc="1차 청킹(마크다운)", unit=" 글"):
         for doc in md_splitter.split_text(detail):
             text = doc.page_content.strip() # 앞뒤공백이 제거된 md제목기준으로 나눈 본문 덩어리
 
@@ -73,6 +74,38 @@ if __name__ == "__main__":
             # (제품아이디, 제품이름, 마크다운 제목, 제목에 해당하는 본문내용)
 
     # 2단계 - 1단계에서 분리한 본문내용의 최대 토큰수용치를 넘어설때 2차 청킹 필요
-    respliter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-        tok, chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP, seperators=SEPERATORS,keep_separator="end"
+    resplitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        tok, chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP, separators=SEPERATORS,keep_separator="end"
     )
+
+    rows = [] #(product_id, product_name, section, chunk_index, body)
+    n_resplit = 0
+
+    for pid, pname, section, text in tqdm.tqdm(sections, desc="2차 청킹(토큰)", unit=" 섹션"):
+        # md파일에서 잘라낸 본문내용이 최대토큰수보다 넘어서면 카운트 1 증가시키면서 2차 청킹작업 시작
+        if ntok(text) >RESPLIT_OVER:
+            n_resplit +=1
+            parts = resplitter.split_text(text)
+        else:
+            parts= [text]
+
+        for i,part in enumerate(parts):
+            rows.append((pid,pname,section, i, part))
+
+    print(rows[0])
+
+    """
+    문자 데이터 청킹 흐름 (보통 실무에서 아래 순서로 작업 프로세스가 고착화되어 있음)
+
+    **필수로 이해해야 되는 개념**
+    1- 일단 마크다운의 제목 구분자로 해서 의미단위로 먼저 짜름 (의미단의)
+    (분기-A) 1차 단계에서 모든 청킹데이터가 최대토큰수안에 들어오면 그냥 무시
+    (분기-B) 1차 단계에서 따른 청킹데이터중 최대토큰을 넘어가는게 있으면 2차 청킹작업 시작
+
+    2- 1차에서 짤린 청킹덩어리중 최대토큰이 넘어가는 덩어리는 다시 반복돌면서 이번엔 문장단위로 청킹시도 (문장단위)
+    3- 2차에서 짤랐는데도 아직도 최대토큰수를 넘어가면 계속 반복돌며 청킹
+
+    **추가적으로 알아두면 좋은 개념**
+    [상품명 > 위치] 본문내용 : 이런식으로 본문앞에 구분자를 붙이는 이유 
+    - LLM 한테 청킹된 데이터를 전달할때 해당 데이터의 제목과 출처를 같이 알려줘서 본문 데이터의 맥락을 파악하게 하기 위함 
+  """
